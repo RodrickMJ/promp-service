62ce5ed22dc43d5306e88b200b4fd146d165b054
RodrickMJ <231199@ids.upchiapas.edu.mx>
2025-12-04 23:25:39 -0600

    adpt vercel.3

--- src/controllers/rag.controller.ts
+++ src/controllers/rag.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { generateRAGQuestions } from "../services/deepseek.service";
+import { generateRAGQuestions } from "../services/deepseek.service.js";
 
 export const RagQuestionController = async (req: Request, res: Response) => {
     try {
